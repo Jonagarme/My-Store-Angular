36014a907d6b0256cd06a0a080657e7b9f29704b
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-07 10:55:40 -0500

    se añadio componente de lista productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,34 +9,14 @@ import { Product } from './models/product.model'
 })
 export class AppComponent {
   imgParent = '';
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/img/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/img/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/img/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/img/books.jpg'
-    },
-  ];
+  showImage = true;
+ 
 
   onLoaded(img: string){
     console.log('log padre', img);
+  }
+
+  toogleImage(){
+    this.showImage
   }
 }
