f98c78cf20735a26bbf9f92973465bbcc099c934
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-07 09:46:53 -0500

    componente producto creado y modificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './models/product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/img/toy.jpg'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/img/bike.jpg'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/img/album.jpg'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/img/books.jpg'
+    },
+  ];
 
   onLoaded(img: string){
     console.log('log padre', img);
-    
   }
 }
