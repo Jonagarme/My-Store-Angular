f503b0feecbb1e53e6b829ddd1433aba2ee2f3a5
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-07 10:13:30 -0500

    añadido ciclos de vida en img.components

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,14 +1,44 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent{
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>()
   imageDefault = './assets/img/default.png'
+
+  constructor(){
+    //before render
+     console.log('constructor', 'imgValue => ', this.img); 
+  }
+
+  ngOnchanges(){
+    //before render
+    //changes inputs --times
+
+    console.log('ngOnchanges', 'imgValue => ', this.img); 
+  }
+
+  ngOnInit(): void {
+    //async - fetch - promises --once time
+
+    console.log('ngOnInit', 'imgValue => ', this.img); 
+  }
+
+  ngAfterViewInit(){
+    //after render
+    //handler children
+
+    console.log('ngAfterViewInit'); 
+  }
+
+  ngOnDestroy() {
+    // delete
+    console.log('ngOnDestroy')
+  }
 
   imgError(){
     this.img = this.imageDefault
