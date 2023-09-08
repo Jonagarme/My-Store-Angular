0118c1bb2e3d751dd88c1bd3b65f2970570417b5
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-08 09:26:04 -0500

    pipes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 import { Product } from '../../models/product.model';
 
-import { StoreService } from '../../services/store.service'
-import { ProductsService } from '../../services/products.service'
+import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -13,27 +13,26 @@ import { ProductsService } from '../../services/products.service'
 export class ProductsComponent {
   myShoppingCart: Product[] = [];
   total = 0;
-  products: Product[] = [ 
-  ];
+  products: Product[] = [];
+  today = new Date();
+  date = new Date(2021, 1, 21)
 
   constructor(
     private storeService: StoreService,
     private productService: ProductsService
-  ){ 
+  ) {
     this.myShoppingCart = this.storeService.getShoppingCart();
-   }
+  }
 
-   ngOnInit(): void{
-    this.productService.getAllProducts()
-    .subscribe(data => {
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((data) => {
       this.products = data;
-      
-    })
-   }
+    });
+  }
 
   onAddToShoppingCart(product: Product) {
     this.myShoppingCart.push(product);
     this.storeService.addProduct(product);
-    this.total = this.storeService.getTotal()
+    this.total = this.storeService.getTotal();
   }
 }
